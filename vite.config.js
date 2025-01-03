bef11bdf677e758d662e0e84872351affc67f19a
madhuniha-dev <boddulurimadhu333@gmail.com>
2025-01-03 23:52:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+    },
+  },
+});
