6a254a4c57941bd647da2823450b4bf8dd500fe2
madhuniha-dev <boddulurimadhu333@gmail.com>
2025-01-03 22:30:12 +0530

    pushing appjs css main js

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Calendar from './components/Calendar/Calendar';
+
+function App() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const handleModalOpen = React.useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleModalClose = React.useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  return (
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Calendar isOpen={isModalOpen} onOpen={handleModalOpen} onClose={handleModalClose} />
+          }
+        />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
