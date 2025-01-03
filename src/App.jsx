cb0a205a74518926573a0afa5bdf9eea06633186
madhuniha-dev <boddulurimadhu333@gmail.com>
2025-01-03 23:52:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Calendar from './components/Calendar/Calendar';
+
+import Calendar from '@components/Calendar/Calendar';
+
 
 function App() {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
